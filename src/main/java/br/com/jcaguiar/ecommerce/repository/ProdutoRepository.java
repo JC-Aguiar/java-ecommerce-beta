cfa84fc185599823834c5f934db29c046b714f40
JC-Aguiar <jcostalaguiar@gmail.com>
2021-06-08 23:12:57 -0300

    Caching

--- src/main/java/br/com/jcaguiar/ecommerce/repository/ProdutoRepository.java
+++ src/main/java/br/com/jcaguiar/ecommerce/repository/ProdutoRepository.java
@@ -2,6 +2,7 @@ package br.com.jcaguiar.ecommerce.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +12,7 @@ import br.com.jcaguiar.ecommerce.model.Categoria;
 import br.com.jcaguiar.ecommerce.model.Produto;
 import br.com.jcaguiar.ecommerce.projection.ProdutoReport;
 
+@Cacheable("Produto")
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
