0892f75ef0d4163a949d4e42874649645ff981be
JC-Aguiar <jcostalaguiar@gmail.com>
2021-07-11 19:23:20 -0300

    New PeiddoItem and PedidoProduto Entities

--- src/main/java/br/com/jcaguiar/ecommerce/model/Pedido.java
+++ src/main/java/br/com/jcaguiar/ecommerce/model/Pedido.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -13,7 +14,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import br.com.jcaguiar.ecommerce.dto.PedidoProdutoDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,12 +34,6 @@ final public class Pedido implements Entidade<Long> {
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
 	
-	@OneToMany(mappedBy = "pedido", fetch = FetchType.LAZY) //PRECISA DE CASCATE?
-	private final List<Pagamento> pagamento = new ArrayList<>();
-	
-	@OneToMany(mappedBy = "pedido", fetch = FetchType.LAZY) //PRECISA DE CASCATE?
-	private final List<NotaFiscal> nf = new ArrayList<>();
-	
 	//INFORMAÇÕES DO PEDIDO
 	private String numero;
 	private short status;
@@ -48,13 +42,8 @@ final public class Pedido implements Entidade<Long> {
 	private LocalDateTime data_pedido = LocalDateTime.now();
 	
 	//INFORMAÇÕES DO PRODUTO
-	private List<String> produtoNome = new ArrayList<>();
-	private List<String> produtoModelo = new ArrayList<>();
-	private List<String> produtoMarca = new ArrayList<>();
-	private List<BigDecimal> produtoValor = new ArrayList<>();
-	private List<String> produtoTamanho = new ArrayList<>();
-	private List<String> produtoMedidas = new ArrayList<>();
-	private List<String> produtoMaterial = new ArrayList<>();
+	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+	private List<PedidoItem> item = new ArrayList<>();
 	
 	//INFORMAÇÕES DO COMPRADOR
 	private String compradorNome;
@@ -65,46 +54,10 @@ final public class Pedido implements Entidade<Long> {
 	private String compradorBairro;
 	private String compradorPais;
 	
-	public void addProduto(String produtoNome, String produtoModelo, String produtoMarca, 
-	BigDecimal produtoValor, String produtoTamanho, String produtoMedidas, String produtoMaterial) {
-		this.produtoNome.add(produtoNome);
-		this.produtoModelo.add(produtoModelo);
-		this.produtoMarca.add( produtoMarca );
-		this.produtoValor.add(produtoValor);
-		this.produtoTamanho.add(produtoTamanho);
-		this.produtoMedidas.add(produtoMedidas);
-		this.produtoMaterial.add(produtoMaterial);
-		quantidadeProdutos++;
-	}
+	@OneToMany(mappedBy = "pedido", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private final List<Pagamento> pagamento = new ArrayList<>();
 	
-	public PedidoProdutoDto getProdutoDto(int index) {
-		PedidoProdutoDto produto = PedidoProdutoDto.builder()
-				.nome( getProdutoNome().get(index) )
-				.modelo( getProdutoModelo().get(index) )
-				.valor( getProdutoValor().get(index) )
-				.tamanho( getProdutoTamanho().get(index) )
-				.medidas( getProdutoMedidas().get(index) )
-				.material( getProdutoMaterial().get(index) )
-				.build();
-		produto.setListaDeMarcas( getProdutoMarca().get(index) );
-		return produto;
-	}
-	
-	public List<PedidoProdutoDto> getAllProdutosDto() {
-		List<PedidoProdutoDto> produtos = new ArrayList<PedidoProdutoDto>();
-		for(int i = 0; i < quantidadeProdutos; i++) {
-			PedidoProdutoDto prod = PedidoProdutoDto.builder()
-						.nome( getProdutoNome().get(i) )
-						.modelo( getProdutoModelo().get(i) )
-						.valor( getProdutoValor().get(i) )
-						.tamanho( getProdutoTamanho().get(i) )
-						.medidas( getProdutoMedidas().get(i) )
-						.material( getProdutoMaterial().get(i) )
-						.build();
-			prod.setListaDeMarcas( getProdutoMarca().get(i) );
-			produtos.add(prod);
-		}
-		return produtos;
-	}
+	@OneToMany(mappedBy = "pedido", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private final List<NotaFiscal> nf = new ArrayList<>();
 	
 }
---
+++ src/main/java/br/com/jcaguiar/ecommerce/model/PedidoItem.java
@@ -0,0 +1,39 @@
+package br.com.jcaguiar.ecommerce.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity(name = "carrinho_item")
+final public class PedidoItem implements Entidade<Long> {
+
+	@Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private PedidoProduto produto;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Pedido pedido;
+	private short quantidade;
+	private BigDecimal total;
+	private final LocalDateTime data_carrinho = LocalDateTime.now();
+	
+}
