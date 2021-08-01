40c2f3a6707b02b888476b0d8f6526a844b276f8
JC-Aguiar <jcostalaguiar@gmail.com>
2021-08-01 17:05:01 -0300

    Adjusting REST login
    Writing better comments for each method

--- src/main/java/br/com/jcaguiar/ecommerce/security/TokenService.java
+++ src/main/java/br/com/jcaguiar/ecommerce/security/TokenService.java
@@ -2,6 +2,7 @@ package br.com.jcaguiar.ecommerce.security;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +14,43 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 
 @Service
+/**
+ * 
+ * @author JM Costal Aguiar
+ *
+ */
 public final class TokenService {
 	/**ATRIBUTOS
 	 * segredo:		assinatura de validação dos tokens
 	 * tempoLogin:	validade do token após criado (em milisegundos)
+	 * log:			para exibir informações no console
+	 * 				(valor coletado no arquivo "application.properties")
 	 */
 	private String segredo = "AAAAB3NzaC1yc2EAAAADAQABAAABAQCu9uKkd/f23+CSmwp/Sx72HkRu1wW5Qn238DRzTW7IZWJi2IruikgxXewhaL9ncS8Bm437ScfmjjewLZuVxyRwMs2vBCb4yuXvYl4v2gd+vjw3QdlpHOplTE3BzA1LPco8vVEevBO9j8vFJoHcYjdwnhaOVqFl2Nm+I2WEBFVlnJtWV/zmdmVZxrCxvYEuZ1kLigfA9dtwtOEWrvcieIg132rB73HgmnjhKUKjBjbXzDEW0drgUnjt/Q8Jr/ix6IgPX6F71V6bwkJb0POv/rOHXOnh8gshgZQMgvrQ9/IFk6Ko+FBtMenqIeEZyNnB0chwo2SPAyOdo5w9y6XxcIQ9 ";
 	private int tempoLogin = 1800000;
+	@Value("${ecommerce.system.log}") private boolean log;
 	
 	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-	/** CRIANDO TOKEN APÓS LOGIN
+	/**CRIANDO TOKEN APÓS LOGIN
+	 * Uma vez autenticado, o token JWT será gerado a este usuário
+	 * 1) Coletando usuário
+	 * 			Authentication:		Classe gerenciada pelo AuthenticationManager (no LoginController) e quando esta mesma
+	 * 								classe é instanciada, recebe a email e senha do usuário.
+	 * 			getPrincipal:		Método que retorna a classe usuário que está salvo no Authentication.
+	 * 
+	 * 2) Definindo datas
+	 * 			hoje:				Data atual.
+	 * 			validade:			Data de expiração do token para esse login.
+	 * 
+	 * 3) Classe Jwt para criação do token:
+	 * 			builder:			Preenchendo campos do construtor Jwts.
+	 * 			setIssuer:			Nome do emissor.
+	 * 			setSubject:			Identificador único (id) do objeto de login (usuário) em formato String.
+	 * 			setIssuedAt:		Data emissáo do token.
+	 * 			setExpiration:		Data de expiração de uso do token.
+	 * 			signWith:			Recebe método de criptografia e assinatura secreta da API.
+	 * 			SignatureAlgorithm:	Método estático do Jwts com criptorgrafias disponíveis.
+	 * 			compact:			Criar token.
 	 * 
 	 * @param userAutenticado
 	 * @return
@@ -43,14 +71,14 @@ public final class TokenService {
 	}
 
 	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-	/** VALIDANDO TOKEN DO REQUEST
+	/**VALIDANDO TOKEN DO REQUEST
 	 * 1) Tentar decodificar token do usuário
-	 * 		Jwts:				classe responsável pelas interfaces Jwt
-	 * 		parser: 			decodificador de token
-	 * 		setSigningKey:	 	definir assinatura do decodificador, 
+	 * 		Jwts:				Classe responsável pelas interfaces Jwt.
+	 * 		parser: 			Decodificador de token.
+	 * 		setSigningKey:	 	Definir assinatura do decodificador, 
 	 * 				  			(tanto o token quanto o sistema precisam ser iguais).
-	 * 		parseClaimsJws: 	converta o playload (usuário)
-	 * 2) 
+	 * 		parseClaimsJws: 	Converta o playload (usuário).
+	 *
 	 */
 	public boolean validar(String token) {
 		try {
@@ -60,23 +88,31 @@ public final class TokenService {
 		}
 		catch (MalformedJwtException e) {
 			//Se o token não estiver no padrão JWS
-			System.out.printf("O token do usuário não é um JWS válido\n");
+			log("O token do usuário não é um JWS válido\n");
 		}
 		catch (SignatureException e) {
 			//Se a assinatura do token não corresponde
-			System.out.printf("Assinatura do token não é compatível\n");
+			log("Assinatura do token não é compatível\n");
 		}
 		catch (ExpiredJwtException e) {
 			//Se o tempo de validade do token expirou
-			System.out.printf("Token com tempo de validade expirado\n");
+			log("Token com tempo de validade expirado\n");
 		}
 		catch (IllegalArgumentException e) {
 			//Se o token está em branco, só com espaços em brancos ou nulo
-			System.out.printf("Token com valores incorretos (vazio, nulo ou em branco)\n");
+			log("Token com valores incorretos (vazio, nulo ou em branco)\n");
 		}
 		return false;
 	}
 	
-	
+	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+	/**EXIBINDO MENSAGENS DO SISTEMA
+	 * Variável local "log" precisa estar ativada 
+	 * @param mensagem
+	 */
+	private void log(String mensagem) {
+		if(log)
+			System.out.printf(mensagem);
+	}
 	
 }
